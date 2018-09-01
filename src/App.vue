91695ca306dabae1665b104dd8674adb1fcc5e0f
backsword <42869557+backswordit@users.noreply.github.com>
2018-09-01 13:37:38 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ export default {
 
 
 
-    // 调用API从本地缓存中获取数据66666yy
+    // 调用API从本地缓存中获取数据777
     // const logs = wx.getStorageSync('logs') || []
     // logs.unshift(Date.now())
     // wx.setStorageSync('logs', logs)
