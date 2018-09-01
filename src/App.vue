582f7a82927d409f7967308b6d437a2a7cef44d9
backsword <backsword@qq.com>
2018-09-01 11:19:17 +0800

    modify at 2018-09-01

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ export default {
 
     
     
-    // 调用API从本地缓存中获取数据11333
+    // 调用API从本地缓存中获取数据113336888
     // const logs = wx.getStorageSync('logs') || []
     // logs.unshift(Date.now())
     // wx.setStorageSync('logs', logs)
